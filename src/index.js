3e0e3068484b037950c6f0d47f9583cded439af8
shethabhish <shethabhish@gmail.com>
2020-04-20 21:10:25 -0700

    Final

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Season from './season';
+import Loader from './loader';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class App extends React.Component {
+  state = { lat: null, error: '' };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  componentDidMount() {
+    // best used to load data initially
+    window.navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.setState({ lat: position.coords.latitude });
+      }, // if success
+      (err) => {
+        this.setState({ error: err.message });
+      }
+    );
+  }
+
+  render() {
+    if (this.state.error && !this.state.lat) {
+      return <div>Error: {this.state.error}</div>;
+    }
+    if (!this.state.error && this.state.lat) {
+      return <Season lat={this.state.lat} />;
+    }
+
+    return (
+      <div>
+        <Loader message="Please, accept location request" />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
